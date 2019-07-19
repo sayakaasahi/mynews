1f2edb666bf1636073913e455f0fe0cf26ff2cf4
asahi <asahi38saya@yahoo.co.jp>
2019-07-19 11:04:47 +0000

    PHP/Laravel11-2

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix'=>'admin'],function(){
-  Route::get('news/create','Admin\NewsController@add');
+  Route::get('news/create','Admin\NewsController@add')->middleware('auth');
 });
 Route::get('admin/profile/create','Admin\ProfileController@add');
 Route::get('admin/profile/edit','Admin\ProfileController@edit');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
