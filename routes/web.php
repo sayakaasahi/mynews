09a4b3587e8793b55aae5183118a2778f7fb59a4
asahi <asahi38saya@yahoo.co.jp>
2019-07-27 04:32:32 +0000

    PHP/Laravel16

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix'=>'admin','middleware'=>'auth'],function() {
-  Route::get('news/create','Admin\NewsController@add');
-  Route::post('news/create','Admin\NewsController@create');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
 Route::group(['prefix'=>'admin','middleware'=>'auth'],function() {
   Route::get('profile/create','Admin\ProfileController@add');
   Route::post('profile/create','Admin\ProfileController@create');
-});
-Route::group(['prefix'=>'admin','middleware'=>'auth'],function() {
   Route::get('profile/edit','Admin\ProfileController@edit');
   Route::post('profile/edit','Admin\ProfileController@update');
 });
